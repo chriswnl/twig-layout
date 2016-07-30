1825e245e915db9a5c4f14f5d1539d58a3262f2e
Chris Willis <chris@twigtelecom.nl>
2016-07-30 22:11:11 +0200

    gmail config updated

--- lib/generators/twig_layout/templates/gmail.rb
+++ lib/generators/twig_layout/templates/gmail.rb
@@ -7,13 +7,21 @@ Rails.application.configure do
     domain:               ENV['GMAIL_DOMAIN'],
     user_name:            ENV['GMAIL_USERNAME'],
     password:             ENV['GMAIL_PASSWORD'],
+    tls:                  true,
+    ssl:                  true,
     authentication:       'plain',
     enable_starttls_auto: true  }
 
   if Rails.env.production?
     config.action_mailer.perform_deliveries = true
     config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.default_url_options = { host: ENV['WEB_ADDRESS'] }
   else
     config.action_mailer.default_url_options = { host: 'localhost:3000' }
   end
 end
+
+if Rails.env.production?
+  Rails.application.routes.default_url_options[:host] = ENV['WEB_ADDRESS']
+end
+
